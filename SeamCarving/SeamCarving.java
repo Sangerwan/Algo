07a767f6f9b8b7b9364cbed211aee8c28b5dc5a5
Sangerwan <sangerwan@gmail.com>
2020-05-20 16:52:08 +0200

    return

--- SeamCarving/SeamCarving.java
+++ SeamCarving/SeamCarving.java
@@ -9,6 +9,7 @@ public class SeamCarving {
 
     public static void ForwardEnergy(BufferedImage img, int x, int y){
         img.getRGB(x, y);
+        return;
     }
     public static void main(String[] args) throws IOException{
         System.out.println(args[0]);
